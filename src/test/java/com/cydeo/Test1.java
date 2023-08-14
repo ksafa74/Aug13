2febef002412695d2ec9a1d2c59a098ad79c6e28
Kasim_Cydeo <safasdet@gmail.com>
2023-08-13 23:25:31 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/Test1.java
@@ -0,0 +1,27 @@
+package com.cydeo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+import java.time.Duration;
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.google.com");
+
+
+
+
+    }
+}
